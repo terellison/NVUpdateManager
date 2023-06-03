fd85c320b4b81c3cd96b42ed15b0276b9c67c396
Terry Ellison <ellisonterry4@gmail.com>
2023-06-03 16:08:37 -0400

    Adding tests (#1)
    * added test projects for EmailHandler and DriverManager
    
    * removed SnedPriority
    refactored EmailInfo properties
    refactored property names for consistency
    
    * removed SendPriority parameter
    added endpoint encryption functionality
    
    * wrote tests for endpoint encryption and getting driver info

---
+++ NVUpdateManager.Core.Tests/DriverManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NVUpdateManager.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVUpdateManager.Core.Tests
+{
+    [TestClass()]
+    public class DriverManagerTests
+    {
+        [TestMethod()]
+        public async Task GetInstalledDriverInfoTest()
+        {
+            DriverInfo? driverData = null;
+
+            try
+            {
+                driverData = await DriverManager.GetInstalledDriverInfo();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(NotSupportedException));
+            }
+
+            Assert.IsNotNull(driverData);
+
+            var properties = driverData.GetType().GetProperties();
+
+            foreach( var property in properties )
+            {
+                Assert.IsNotNull(property.GetValue(driverData));
+            }
+        }
+    }
+}
---
+++ NVUpdateManager.EmailHandler.Tests/EmailHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static NVUpdateManager.EmailHandler.EmailHandler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVUpdateManager.EmailHandler.Tests
+{
+    [TestClass()]
+    public class EmailHandlerTests
+    {
+        [TestMethod()]
+        public void EncodeLogicAppEndpointTest()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            EncodeLogicAppEndpoint("foo");
+            var output = writer.ToString();
+
+            Assert.IsTrue(output.Contains("entropy"));
+            Assert.IsTrue(output.Contains("encrypted endpoint"));
+
+            var data = output.Split('\n');
+
+            var entropyLength = data[0].LastIndexOf('\"') - data[0].IndexOf('\"');
+            var encryptedEndpointLength = data[1].LastIndexOf('\"') - data[1].IndexOf('\"');
+
+            string entropy = data[0].Substring(data[0].IndexOf('\"') + 1, entropyLength - 1);
+            string encryptedEndpoint = data[1].Substring(data[1].IndexOf('\"') + 1, encryptedEndpointLength - 1);
+
+            var result = DecodeSecureEndpoint(encryptedEndpoint, entropy);
+
+            Assert.IsTrue(result.Equals("foo"));
+        }
+    }
+}
