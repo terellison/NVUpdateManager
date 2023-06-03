a07238d6b809e07f47d4979db5d35b8af99222a6
Terry Ellison <ellisonterry4@gmail.com>
2023-06-03 12:16:11 -0400

    added test projects for EmailHandler and DriverManager

---
+++ NVUpdateManager.Core.Tests/DriverManagerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVUpdateManager.Core.Tests
+{
+    internal class DriverManagerTests
+    {
+    }
+}
---
+++ NVUpdateManager.EmailHandler.Tests/EmailHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NVUpdateManager.EmailHandler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVUpdateManager.EmailHandler.Tests
+{
+    [TestClass()]
+    public class EmailHandlerTests
+    {
+        [TestMethod()]
+        public void ConfigureLogicAppEndpointTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EncodeLogicAppEndpointTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecodeSecureEndpointTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendNotificationEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfigureAddressesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
