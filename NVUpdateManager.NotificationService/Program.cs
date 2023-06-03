296f053a890f054846e0a684e807dffc576b0a91
Terry Ellison <ellisonterry4@gmail.com>
2023-06-03 18:16:06 -0400

    adding installer for NotificationService (#2)
    * added install and uninstall logic
    
    * added installer project for NotificationService
    
    * fixed usage message

--- NVUpdateManager.NotificationService/Program.cs
+++ NVUpdateManager.NotificationService/Program.cs
@@ -1,19 +1,25 @@
 using NVUpdateManager.NotificationService.Data;
 using static NVUpdateManager.EmailHandler.EmailHandler;
+using CliWrap;
 
 namespace NVUpdateManager.NotificationService
 {
     public class Program
     {
+        private const string ServiceName = $"{nameof(NVUpdateManager)}.{nameof(NotificationService)}";
         private static string Usage =
             @"
                 Run with no arguments to start the service normally
 
                 Usage:
                 
-                    -EncryptEndpoint: Encrypt Azure Logic App endpoint
+                    /EncryptEndpoint: Encrypt Azure Logic App endpoint
 
-                    Example: NVUpdateManager.NotificationService.exe -EncryptEndpoint ""your-endpoint-here""
+                    Example: NVUpdateManager.NotificationService.exe /EncryptEndpoint ""your-endpoint-here""
+
+                    /Install: install as a service
+
+                    /Uninstall: Uinstall existing service
             ";
 
         public static void Main(string[] args)
@@ -26,7 +32,7 @@ namespace NVUpdateManager.NotificationService
             IHost host = Host.CreateDefaultBuilder(args)
                 .UseWindowsService(options =>
                 {
-                    options.ServiceName = $"{nameof(NVUpdateManager)}.{nameof(NotificationService)}";
+                    options.ServiceName = ServiceName;
                 })
                 .ConfigureServices(( hostContext, services) =>
                 {
@@ -59,14 +65,41 @@ namespace NVUpdateManager.NotificationService
 
         private static void ParseArguments(string[] args)
         {
-            if (args[0].ToLower().Equals("-encryptendpoint"))
+            switch(args[0].ToLower())
             {
-                EncodeLogicAppEndpoint(args[1]);
+                case "/encryptendpoint":
+                    EncodeLogicAppEndpoint(args[1]);
+                    break;
+                case "/install":
+                    _ = Install();
+                    break;
+                case "/uninstall":
+                    _ = Uninstall();
+                    break;
+                default:
+                    ShowUsage();
+                    break;
             }
-            else
-            {
-                ShowUsage();
-            }
+        }
+
+        private static async Task Install()
+        {
+            var executablePath = Path.Combine(AppContext.BaseDirectory, $"{ServiceName}.exe");
+
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
+                .ExecuteAsync();
+        }
+
+        private static async Task Uninstall()
+        {
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "stop", ServiceName })
+                .ExecuteAsync();
+
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "delete", ServiceName })
+                .ExecuteAsync();
         }
 
         private static void ShowUsage()
