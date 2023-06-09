2bc38a9bb13629b6638ddf5c0c742d979888a5df
terellison <ellisonterry4@gmail.com>
2023-06-09 16:36:12 -0400

    added ServiceCollection extension method for DriverManager

---
+++ NVUpdateManager.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NVUpdateManager.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVUpdateManager.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDriverManager(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IDriverManager, DriverManager>();
+            return services;
+        }
+    }
+}
