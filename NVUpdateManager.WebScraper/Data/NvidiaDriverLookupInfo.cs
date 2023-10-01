2def1394d8cecc5780633bac149b374661c1d37d
terellison <ellisonterry4@gmail.com>
2023-10-01 12:54:07 -0400

    started process of using API call instead of Selenium

---
+++ NVUpdateManager.WebScraper/Data/NvidiaDriverLookupInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVUpdateManager.WebScraper.Data
+{
+    internal static class NvidiaDriverLookupInfo
+    {
+        internal static Dictionary<string, int> GetProductSeriesSearchValue()
+        {
+            return new Dictionary<string, int>
+            {
+                { "GeForce", 120 }
+            };
+        }
+
+        internal static Dictionary<string, int> GetProductFamilySearchValue()
+        {
+            return new Dictionary<string, int>
+            {
+                { "GeForce RTX 3080", 929 }
+            };
+        }
+
+        internal static int OperatingSystemSearchValue = 135;
+
+        internal static int LanguageSearchValue = 1;
+    }
+}
--- NVUpdateManager.WebScraper/UpdateFinder.cs
+++ NVUpdateManager.WebScraper/UpdateFinder.cs
@@ -1,98 +1,23 @@
 using System;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Threading;
 using NVUpdateManager.WebScraper.Data;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Support.UI;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
+using static NVUpdateManager.WebScraper.Data.NvidiaDriverLookupInfo;
 
 namespace NVUpdateManager.WebScraper
 {
     public static class UpdateFinder
     {
-        private const string _searchUrl = "https://www.nvidia.com/Download/Find.aspx";
-        private static IWebDriver _driver;
-
         public static UpdateInfo FindLatestUpdate(string gpuSeries, string gpuName, string driverType)
         {
-            var options = new EdgeOptions();
+            // TODO: Deduce arguments, make api call, parse the HTML result
 
-            options.AddArgument("--no-sandbox");
-            options.AddArguments("--disable-dev-shm-usage");
+            var productSeriesId = GetProductSeriesSearchValue()[gpuSeries];
 
-            var file = new DriverManager().SetUpDriver(new EdgeConfig(), "Latest");
-
-            _driver = new EdgeDriver(Path.GetDirectoryName(file), options);
-
-            UpdateInfo info;
-
-            try
-            {
-                _driver.Url = _searchUrl;
-
-                SelectOptionFromSelector("selProductSeriesType", driverType);
-
-                SelectOptionFromSelector("selProductSeries", gpuSeries);
-
-                SelectOptionFromSelector("selProductFamily", gpuName);
-
-                SelectOptionFromSelector("ddWHQL", "Recommended/Certified"); // We only want recommended drivers
-
-                var searchButton = _driver.FindElement(By.CssSelector("btn_drvr_lnk_txt"));
-
-                searchButton.Click();
-
-                Thread.Sleep(500); // We should use an event to know when the page is ready instead of waiting
-
-                NavigateToLatestDriver();
-
-                info = GetLatestDriverInfo();
-            }
-            finally { _driver.Quit(); }
-
-            _driver.Dispose();
+            var productFamilyId = GetProductFamilySearchValue()[gpuName];
 
 
-            return info;
-        }
-
-        private static UpdateInfo GetLatestDriverInfo()
-        {
-            var versionNumber = _driver.FindElement(By.Id("tdVersion")).Text
-                                       .Split(' ')
-                                       .First();
-            var releaseDate = _driver.FindElement(By.Id("tdReleaseDate")).Text;
-            var details = _driver.FindElement(By.Id("tab1_content"))
-                                 .GetAttribute("innerHTML")
-                                 .Trim();
-
-            _driver.Url = _driver.FindElement(By.Id("lnkDwnldBtn")).GetAttribute("href");
-
-            var downloadLink = _driver.FindElements(By.CssSelector("a"))
-                                      .FirstOrDefault(x => x.GetAttribute("innerHTML").Contains("btn_drvr_lnk_txt"))
-                                      .GetAttribute("href");
-
-            return new UpdateInfo(versionNumber, releaseDate, details, downloadLink);
-        }
-
-        private static void NavigateToLatestDriver()
-        {
-            var latestDriver = _driver.FindElement(By.Id("driverList"))
-                                      .FindElements(By.CssSelector("td"))
-                                      .FirstOrDefault(x => x.GetAttribute("class") == "gridItem driverName");
-
-            latestDriver?.FindElement(By.CssSelector("a")).Click();
-        }
-
-        private static void SelectOptionFromSelector(string selectorId, string toSelect)
-        {
-            var selector = new SelectElement(_driver.FindElement(By.Id(selectorId)));
-
-            selector.SelectByText(toSelect);
+            throw new NotImplementedException();
         }
 
         public static string DownloadUpdate(string updateLink)
