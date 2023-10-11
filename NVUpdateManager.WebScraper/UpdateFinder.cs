d13ab009171e2d4c0c56abd9b311d65fa909f1bf
terellison <ellisonterry4@gmail.com>
2023-10-11 16:09:59 -0400

    using AngleSharp to parse html response data

--- NVUpdateManager.WebScraper/UpdateFinder.cs
+++ NVUpdateManager.WebScraper/UpdateFinder.cs
@@ -24,15 +24,17 @@ namespace NVUpdateManager.WebScraper
 
             var initialURI = $"https://www.nvidia.com/Download/processFind.aspx?psid={productSeriesId}&pfid={productFamilyId}&osid=57&lid=1&whql=&lang=en-us&ctk=0&qnfslb=00&dtcid=1";
 
+            var updateHtml = string.Empty;
             using(var client = new HttpClient())
             {
                 var driverListResponse = await client.GetAsync(initialURI);
 
-                ParseLinkToUpdate(await driverListResponse.Content.ReadAsStringAsync());
+                var latestUpdateLink = ParseLinkToUpdate(await driverListResponse.Content.ReadAsStringAsync());
 
+                updateHtml = await (await client.GetAsync(latestUpdateLink)).Content.ReadAsStringAsync();
             }
 
-            throw new NotImplementedException();
+            return await ParseUpdateInfo(updateHtml);
         }
 
         private static string ParseLinkToUpdate(string html)
@@ -41,10 +43,58 @@ namespace NVUpdateManager.WebScraper
 
             var updateTable = parser.ParseDocument(html);
 
-            _ = updateTable.QuerySelectorAll("td")
-                .Where(x => x.Id == "driverList");
+            var latestDriver = updateTable.All.First(x => x.Id == "driverList");
 
-            return string.Empty;
+            var result = "https:";
+            result += latestDriver.QuerySelector("a").GetAttribute("href");
+
+            return result;
+        }
+
+        private static async Task<UpdateInfo> ParseUpdateInfo(string html)
+        {
+            var parser = new HtmlParser();
+
+            var updatePage = parser.ParseDocument(html);
+
+            var versionNumber = updatePage.QuerySelectorAll("td")
+                .FirstOrDefault(x => x.Id == "tdVersion")
+                .Text()
+                .Trim();
+
+            versionNumber = versionNumber.Substring(0, versionNumber.IndexOf('W')).TrimEnd();
+
+
+            var releaseDate = updatePage.All.First(x => x.Id == "tdReleaseDate").InnerHtml.Trim();
+            var details = updatePage.All.First(x => x.Id == "tab1_content").InnerHtml.Trim();
+
+            var url = "https://nvidia.com";
+                
+            url += updatePage.QuerySelectorAll("a")
+                             .FirstOrDefault(x => x.InnerHtml.Contains("btn_drvr_lnk_txt"))
+                             .GetAttribute("href");
+
+            url = await GetActualDownloadLink(url);
+
+            return new UpdateInfo(versionNumber, releaseDate, details, url);
+        }
+
+        private static async Task<string> GetActualDownloadLink(string url)
+        {
+            HttpResponseMessage response;
+
+            using(var client  = new HttpClient())
+            {
+                response = await client.GetAsync(url);
+            }
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            var downloadPage = new HtmlParser().ParseDocument(html);
+
+            var result = "https:";
+
+            return result + downloadPage.QuerySelector("btn_drvr_lnk_txt").ParentElement.GetAttribute("href");
         }
 
         public static string DownloadUpdate(string updateLink)
